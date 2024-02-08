4bc3ea8343fdba3d433b77cc98a5cec9cb23d001
Mehebul02 <mehebul2123@gmail.com>
2024-02-08 23:21:41 +0600

    first commit

---
+++ object.js
@@ -0,0 +1,8 @@
+const laptop ={
+    brand:"Hp",
+    model:"hp2690aq",
+    price:61000,
+    color:"White",
+    warentry :"2 Year"
+}
+console.log(laptop)
---
+++ property.js
@@ -0,0 +1,12 @@
+const person = {
+    nam :"Mehebul Alif",
+    age:20,
+    salary:2500,
+    profession:"developer",
+    married:true
+    
+
+}
+person.salary=3008
+person['age']=30;
+console.log(person)
